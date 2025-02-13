c48502ab6e6c59be5d43a8eb411d96b8f3eff2d6
Lailatul Husna Monika <u1904009@student.cuet.ac.bd>
2025-02-13 22:11:54 +0600

    Update sequencer.js

--- sequencer.js
+++ sequencer.js
@@ -2,17 +2,14 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 
-// Redis ক্লায়েন্ট তৈরি করুন
 const client = redis.createClient();
 
 client.on('error', (err) => {
     console.error('Redis error:', err);
 });
 
-// Unique ID জেনারেট করার এন্ডপয়েন্ট
 app.get('/get-id', async (req, res) => {
     try {
-        // Redis-এ Atomic Operation ব্যবহার করে ID জেনারেট করুন
         const id = await client.incr('unique_id_counter');
         res.json({ id });
     } catch (error) {
